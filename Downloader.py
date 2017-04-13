adbb7696057abc189c82f50447a4ce1d2643222b
Rishabh Shah <rishabhshah28@gmail.com>
2017-04-12 22:40:40 -0400

    Download image from Google Drive then deletes image from Google Drive.

--- Downloader.py
+++ Downloader.py
@@ -18,27 +18,33 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+# Imports
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 import re
 
+#Google Drive authentication stuff
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
 
 drive = GoogleDrive(gauth) # Create GoogleDrive instance with authenticated GoogleAuth instance
 
+#Designates Google Drive Folder
 folderID = '0B77gCYHoIAkAd29sREZBNlhPclk'
 file_list = drive.ListFile({'q': "'%s' in parents and trashed=false" % folderID}).GetList()
 
+#Writes the Google Drive information to a text file
 fileNamesWrite = open('fileNames.txt', 'wr')
 
 for item in file_list:
 	fileNamesWrite.write(str(item) + "\n")
 fileNamesWrite.close()
 
+#Reads the text file to get file IDs and names
 fileNamesRead = open('fileNames.txt', 'r')
 fileNamesReadRow = fileNamesRead.readlines()
 
+#Get the file IDs
 IDList = []
 for line in fileNamesReadRow:
 	start = line.index("id': u'") + len("id': u'")
@@ -46,14 +52,18 @@ for line in fileNamesReadRow:
 	print(line[start:end])
 	IDList.append(line[start:end])
 
+#Get the filenames
 nameList = []
 for line in fileNamesReadRow:
 	start = line.index("originalFilename': u'") + len("originalFilename': u'")
 	end = line.index("',", start)
 	nameList.append(line[start:end])
 
+#Remove the .jpg from the filename
 for item in nameList:
 	item = item[:-4]
 
+#Download the first image from the folder and then delete from Google Drive
 imageToDownload = drive.CreateFile({'id': IDList[0]})
 imageToDownload.GetContentFile('imageToUpload.jpg')
+imageToDownload.Delete()
